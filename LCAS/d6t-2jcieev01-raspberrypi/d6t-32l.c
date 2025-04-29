bf7e4db49bcb3d608ccd9ae7372b08ba8c048c4b
Elizar <elizarvtsy@gmail.com>
2025-04-29 14:52:20 -0400

    Updated the D6T-32L C file to include the I2C mux

--- LCAS/d6t-2jcieev01-raspberrypi/d6t-32l.c
+++ LCAS/d6t-2jcieev01-raspberrypi/d6t-32l.c
@@ -34,6 +34,9 @@
 #include <stdbool.h>
 #include <time.h>
 #include <linux/i2c.h> //add
+#include <wiringPi.h>
+
+
 
 /* defines */
 #define D6T_ADDR 0x0A  // for I2C 7bit address
@@ -41,10 +44,9 @@
 #define D6T_SET_ADD 0x01
 
 // for use with PCA9546 4-channel I2C multiplexer.
-#define D6T_1 0x70 
-#define D6T_2 0x71
-#define D6T_3 0x72
-#define D6T_4 0x73
+#define A0_PIN 0  // GPIO 17
+#define A1_PIN 1  // GPIO 18
+#define A2_PIN 2  // GPIO 27
 
 #define N_ROW 32
 #define N_PIXEL (32 * 32)
@@ -64,50 +66,92 @@ double pix_data[N_PIXEL];
 /* I2C functions */
 /** <!-- i2c_read_reg8 {{{1 --> I2C read function for bytes transfer.
  */
-uint32_t i2c_read_reg8(uint8_t devAddr, uint8_t regAddr,
-	uint8_t *data, int length
-) {
-	int fd = open(I2CDEV, O_RDWR);
-	if (fd < 0) {
-		fprintf(stderr, "Failed to open device: %s\n", strerror(errno));
-		return 21;
-	}
-	int err = 0;
-	do {
-		struct i2c_msg messages[] = {
-			{ devAddr, 0, 1, &regAddr },
-			{ devAddr, I2C_M_RD, length, data },
-		};
-		struct i2c_rdwr_ioctl_data ioctl_data = { messages, 2 };
-		if (ioctl(fd, I2C_RDWR, &ioctl_data) != 2) {
-			fprintf(stderr, "i2c_read: failed to ioctl: %s\n", strerror(errno));
-		}
 
-	} while (false);
-	close(fd); //change
-	return err;
+ void select_channel(uint8_t channel) {
+    switch (channel) {
+        case 0:
+            digitalWrite(A0_PIN, LOW);
+            digitalWrite(A1_PIN, LOW);
+            digitalWrite(A2_PIN, LOW);
+            break;
+        case 1:
+            digitalWrite(A0_PIN, HIGH);
+            digitalWrite(A1_PIN, LOW);
+            digitalWrite(A2_PIN, LOW);
+            break;
+        case 2:
+            digitalWrite(A0_PIN, LOW);
+            digitalWrite(A1_PIN, HIGH);
+            digitalWrite(A2_PIN, LOW);
+            break;
+        case 3:
+            digitalWrite(A0_PIN, HIGH);
+            digitalWrite(A1_PIN, HIGH);
+            digitalWrite(A2_PIN, LOW);
+            break;
+        default:
+            fprintf(stderr, "Invalid channel selection!\n");
+            break;
+    }
 }
 
-uint32_t i2c_write_reg8(uint8_t devAddr,
-                        uint8_t *data, int length) {
+/* Adjusted to incorporate select_channel function*/
+uint32_t i2c_read_reg8(uint8_t channel, uint8_t devAddr, uint8_t regAddr,
+    uint8_t *data, int length) {
+
     int fd = open(I2CDEV, O_RDWR);
     if (fd < 0) {
         fprintf(stderr, "Failed to open device: %s\n", strerror(errno));
         return 21;
     }
+
+    
+    select_channel(channel);
+
     int err = 0;
     do {
-        if (ioctl(fd, I2C_SLAVE, devAddr) < 0) {
-            fprintf(stderr, "Failed to select device: %s\n", strerror(errno));
-            err = 22; break;
+        struct i2c_msg messages[] = {
+            { devAddr, 0, 1, &regAddr },
+            { devAddr, I2C_M_RD, length, data },
+        };
+        struct i2c_rdwr_ioctl_data ioctl_data = { messages, 2 };
+        if (ioctl(fd, I2C_RDWR, &ioctl_data) != 2) {
+            fprintf(stderr, "i2c_read: failed to ioctl: %s\n", strerror(errno));
         }
-        if (write(fd, data, length) != length) {
-            fprintf(stderr, "Failed to write reg: %s\n", strerror(errno));
-            err = 23; break;
-        }
+
     } while (false);
+
     close(fd);
     return err;
+}
+
+/* Adjusted to incorporate select_channel function*/
+uint32_t i2c_write_reg8(uint8_t channel, uint8_t devAddr,
+						uint8_t *data, int length) {
+
+	int fd = open(I2CDEV, O_RDWR);
+	if (fd < 0) {
+		fprintf(stderr, "Failed to open device: %s\n", strerror(errno));
+		return 21;
+	}
+
+
+	select_channel(channel);
+
+	int err = 0;
+	do {
+		if (ioctl(fd, I2C_SLAVE, devAddr) < 0) {
+			fprintf(stderr, "Failed to select device: %s\n", strerror(errno));
+			err = 22; break;
+		}
+		if (write(fd, data, length) != length) {
+			fprintf(stderr, "Failed to write reg: %s\n", strerror(errno));
+			err = 23; break;
+		}
+	} while (false);
+
+	close(fd);
+	return err;
 }
 
 uint8_t calc_crc(uint8_t data) {
@@ -159,52 +203,70 @@ void initialSetting(void) {
     i2c_write_reg8(D6T_ADDR, dat1, sizeof(dat1));
 }
 
+/* Initializing GPIO for selecting channels on te PCA9546*/
+void setup_gpio() {
+    // Initialize the wiringPi library
+    if (wiringPiSetup() == -1) {
+        fprintf(stderr, "WiringPi setup failed: %s\n", strerror(errno));
+        exit(1);
+    }
+    
+    // Set the GPIO pins for A0, A1, A2 as outputs
+    pinMode(A0_PIN, OUTPUT);
+    pinMode(A1_PIN, OUTPUT);
+    pinMode(A2_PIN, OUTPUT);
+}
+
 /** <!-- main - Thermal sensor {{{1 -->
  * 1. Initialize.
  * 2. Read data
  */
-int main() {
+ int main() {
     int i;
-	int16_t itemp;
-	
-	delay(350);	
-	// 1. Initialize
-	initialSetting();
-    delay(390);	
-	
-	while(1){
-		// 2. Read data
-		// Read data via I2C
-		memset(rbuf, 0, N_READ);
-		for (i = 0; i < 10; i++) {
-			uint32_t ret = i2c_read_reg8(D6T_ADDR, D6T_CMD, rbuf, N_READ);
-			if (ret == 0) {
-				break;
-			}
-			else if (ret == 23) {  // write error
-				delay(60);
-			}
-			else if (ret == 24) {  // read error
-				delay(60);
-			}
-		}
-		D6T_checkPEC(rbuf, N_READ - 1);
-		
-        //Convert to temperature data (degC)
-		ptat = (double)conv8us_s16_le(rbuf, 0) / 10.0;
-		for (i = 0; i < N_PIXEL; i++) {
-			itemp = conv8us_s16_le(rbuf, 2 + 2*i);
-			pix_data[i] = (double)itemp / 10.0;
-		}
-		
-        //Output results		
-		printf("PTAT: %4.1f [degC], Temperature: ", ptat);
-		for (i = 0; i < N_PIXEL; i++) {
-		    printf("%4.1f, ", pix_data[i]);
-		}
-		printf("[degC]\n");
-		
-		delay(200);
-	}
+    int16_t itemp;
+    uint8_t rbuf[N_READ];
+
+    // Initialize GPIO for channel control
+    setup_gpio();
+
+    delay(350);    
+    // 1. Initialize
+    initialSetting();
+    delay(390);    
+    
+    while (1) {
+        // Loop over all 4 channels for each D6T sensor
+        for (i = 0; i < 4; i++) {
+            // 2. Read data from each sensor
+            memset(rbuf, 0, N_READ);
+            
+            // Read data via I2C
+            for (int retry = 0; retry < 10; retry++) {
+                uint32_t ret = i2c_read_reg8(i, D6T_ADDR, D6T_CMD, rbuf, N_READ);
+                if (ret == 0) {
+                    break;
+                } else {
+                    delay(60);
+                }
+            }
+            
+            D6T_checkPEC(rbuf, N_READ - 1);
+            
+            // Convert to temperature data (degC)
+            ptat = (double)conv8us_s16_le(rbuf, 0) / 10.0;
+            for (int j = 0; j < N_PIXEL; j++) {
+                itemp = conv8us_s16_le(rbuf, 2 + 2 * j);
+                pix_data[j] = (double)itemp / 10.0;
+            }
+            
+            // Output results
+            printf("Sensor %d - PTAT: %4.1f [degC], Temperature: ", i+1, ptat);
+            for (int j = 0; j < N_PIXEL; j++) {
+                printf("%4.1f, ", pix_data[j]);
+            }
+            printf("[degC]\n");
+        }
+        
+        delay(200);
+    }
 }
-// vi: ft=c:fdm=marker:et:sw=4:tw=80
