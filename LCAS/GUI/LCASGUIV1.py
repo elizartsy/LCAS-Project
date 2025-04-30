5bd718189fd403e0fff0fa499e757787e75ad5f2
Elizar <elizarvtsy@gmail.com>
2025-04-30 13:27:27 -0400

    updated GUI

--- LCAS/GUI/LCASGUIV1.py
+++ LCAS/GUI/LCASGUIV1.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'LCAS-GUI-V1QsoGoO.ui'
+## Form generated from reading UI file 'LCAS-GUI-V1lQXBgF.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.0
 ##
@@ -11,14 +11,13 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
-    QCursor, QFont, QFontDatabase, QGradient,
-    QIcon, QImage, QKeySequence, QLinearGradient,
-    QPainter, QPalette, QPixmap, QRadialGradient,
-    QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QMainWindow, QMenu,
-    QMenuBar, QSizePolicy, QStatusBar, QTextBrowser,
-    QWidget)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDial, QFrame, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QTextBrowser, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -47,24 +46,32 @@ class Ui_MainWindow(object):
         self.frame_3.setFrameShadow(QFrame.Shadow.Raised)
         self.frame_4 = QFrame(self.centralwidget)
         self.frame_4.setObjectName(u"frame_4")
-        self.frame_4.setGeometry(QRect(610, 240, 161, 141))
+        self.frame_4.setGeometry(QRect(610, 240, 120, 80))
         self.frame_4.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame_4.setFrameShadow(QFrame.Shadow.Raised)
+        self.textBrowser_2 = QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setObjectName(u"textBrowser_2")
+        self.textBrowser_2.setGeometry(QRect(130, 40, 131, 31))
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(80, 80, 101, 41))
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(200, 80, 101, 41))
+        self.textBrowser_3 = QTextBrowser(self.centralwidget)
+        self.textBrowser_3.setObjectName(u"textBrowser_3")
+        self.textBrowser_3.setGeometry(QRect(130, 140, 131, 31))
+        self.dial = QDial(self.centralwidget)
+        self.dial.setObjectName(u"dial")
+        self.dial.setGeometry(QRect(170, 180, 50, 51))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 800, 22))
-        self.menuMonitoring = QMenu(self.menubar)
-        self.menuMonitoring.setObjectName(u"menuMonitoring")
-        self.menuControl = QMenu(self.menubar)
-        self.menuControl.setObjectName(u"menuControl")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
-        self.menubar.addAction(self.menuMonitoring.menuAction())
-        self.menubar.addAction(self.menuControl.menuAction())
 
         self.retranslateUi(MainWindow)
 
@@ -81,7 +88,24 @@ class Ui_MainWindow(object):
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Thermal Camera Output</p></body></html>", None))
-        self.menuMonitoring.setTitle(QCoreApplication.translate("MainWindow", u"Monitor", None))
-        self.menuControl.setTitle(QCoreApplication.translate("MainWindow", u"Control", None))
+        self.textBrowser_2.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Seed Diode Control</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Diode: ON/OFF", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"TEC: ON/OFF", None))
+        self.textBrowser_3.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Staged Power Supplies</p></body></html>", None))
     # retranslateUi
 
