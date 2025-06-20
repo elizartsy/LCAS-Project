46a77e8801d9db711bc2a7eb7cbae57d02c67f7e
Elizar <elizarvtsy@gmail.com>
2025-06-20 13:08:07 -0400

    Update ThermalWorker.cpp

--- LCAS-Interface/ThermalWorker.cpp
+++ LCAS-Interface/ThermalWorker.cpp
@@ -6,25 +6,34 @@
 extern ThermalCameraManager thermalManager;
 
 ThermalWorker::ThermalWorker(int cameraIndex, QObject *parent)
-    : QObject(parent), camIndex(cameraIndex) {
-    captureTimer = new QTimer(this);
-    captureTimer->setInterval(50);  // 20 FPS approx
-
-    connect(captureTimer, &QTimer::timeout, this, &ThermalWorker::process);
-}
+    : QObject(parent), camIndex(cameraIndex), captureTimer(nullptr) {}
 
 void ThermalWorker::start() {
+    qDebug() << "ThermalWorker::start() called for cam" << camIndex;
+
+    captureTimer = new QTimer(this);  // now created in correct thread
+    captureTimer->setInterval(50);
+    connect(captureTimer, &QTimer::timeout, this, &ThermalWorker::process);
     captureTimer->start();
+
+    qDebug() << "Timer started in thread:" << QThread::currentThread();
 }
 
 void ThermalWorker::stop() {
-    captureTimer->stop();
+    if (captureTimer) {
+        captureTimer->stop();
+        captureTimer->deleteLater();
+        captureTimer = nullptr;
+    }
 }
 
 void ThermalWorker::process() {
     cv::Mat frame = thermalManager.getThermalFrame(camIndex);
+    if (frame.empty()) {
+        qDebug() << "Camera" << camIndex << "returned empty frame.";
+        return;
+    }
     bool triggered = thermalManager.checkAndSaveIfThresholdExceeded(camIndex, frame);
     qDebug() << "ThermalWorker emitting frameReady for cam" << camIndex << ", triggered =" << triggered;
-    emit frameReady(camIndex, frame, triggered);
-
+    emit frameReady(camIndex, frame.clone(), triggered);  // safer with clone
 }
