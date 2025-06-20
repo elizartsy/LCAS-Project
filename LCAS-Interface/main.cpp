73aed5cf21da049e889866b6a5b40ec73526bbb9
Elizar <elizarvtsy@gmail.com>
2025-06-20 13:19:26 -0400

    Update main.cpp

--- LCAS-Interface/main.cpp
+++ LCAS-Interface/main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
+#include <QMetaType>
 #include "mainwindow.h"
 #include "ThermalCameraManager.h"
 
 ThermalCameraManager thermalManager;
 
 int main(int argc, char *argv[]) {
+    qRegisterMetaType<cv::Mat>("cv::Mat");
     QApplication app(argc, argv);
 
     MainWindow window;
