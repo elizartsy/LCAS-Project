41d37afcafe7e62753f3a4dd491329658ec59f13
Elizar <elizarvtsy@gmail.com>
2025-06-20 11:05:23 -0400

    Update ThermalWorker.cpp

--- LCAS-Interface/ThermalWorker.cpp
+++ LCAS-Interface/ThermalWorker.cpp
@@ -23,5 +23,7 @@ void ThermalWorker::stop() {
 void ThermalWorker::process() {
     cv::Mat frame = thermalManager.getThermalFrame(camIndex);
     bool triggered = thermalManager.checkAndSaveIfThresholdExceeded(camIndex, frame);
+    qDebug() << "ThermalWorker emitting frameReady for cam" << camIndex << ", triggered =" << triggered;
     emit frameReady(camIndex, frame, triggered);
+
 }
